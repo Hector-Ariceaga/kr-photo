7d44a6478e2810231a895156a3cd07025d5e8e5e
TecHector <hectorariceaga1990@gmail.com>
2019-06-02 23:53:02 -0700

    Stub out remaining containers, import rest of routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './containers/Home';
+import Sessions from './containers/Sessions'
+import Portfolio from './containers/Portfolio'
+import About from './containers/About'
+import Contact from './containers/Contact'
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import { createGlobalStyle } from "styled-components";
@@ -24,6 +28,10 @@ function App() {
 
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path='/sessions' component={Sessions}>Sessions</Route>
+          <Route exact path='/portfolio' component={Portfolio}>Portfolio</Route>
+          <Route exact path='/about' component={About}>About Us</Route>
+          <Route exact path='/contact' component={Contact}>Contact Us</Route>
         </Switch>
 
         <Footer />
